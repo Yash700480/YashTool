ce05cd5f96f5319607921cd98e5c08b6ee88a512
Yash700480 <100418837+Yash700480@users.noreply.github.com>
2022-03-15 06:49:28 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,23 @@
+
+print("██╗░░░██╗░█████╗░░██████╗██╗░░██╗ ")
+print("╚██╗░██╔╝██╔══██╗██╔════╝██║░░██║ ")
+print("░╚████╔╝░███████║╚█████╗░███████║ ")
+print("░░╚██╔╝░░██╔══██║░╚═══██╗██╔══██║ ")
+print("░░░██║░░░██║░░██║██████╔╝██║░░██║ ")
+print("░░░╚═╝░░░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝ ")
+
+print("Welcome to Yash's Tool")
+print("Best Hacking Tool downloader")
+print("Choose Option")
+print("1. Metasploit")
+print("2. Z-phisher")
+print("3. Coming Soon!")
+a=int(input("Input"))
+if a=="1":
+  print("Coming Soon!")
+elif a=="2":
+  print("Coming Soon!")
+else:
+  print("Enter a valid number!")
+
+
